bc74ea0e2887565eccb7920d54df24e9172ea93b
ilyas <ilyas@mewahniagajaya.com>
2020-12-01 11:51:54 +0700

    backend project laravel

---
+++ README.md
@@ -0,0 +1,1 @@
+## Backend Nurul Imam
---
+++ docker/mysql/init_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customers (
+'id' bigint(20) AUTO_INCREMENT PRIMARY KEY,
+'customer_name' VARCHAR(50)
+);
+
+INSERT INTO 'customers' VALUES ('udong');
